b9b9f57189db0c0b31e34717182e2c4b96dfa102
Jane Ferreira <janymeryc@gmail.com>
2021-03-17 18:05:32 -0300

    criando seção 7

---
+++ Section7/section7_exe01.py
@@ -0,0 +1,18 @@
+/*
+Section7_exe01
+
+Faca um algoritmo que determine o maior entre N números.
+A condição de parada é a entrada de um valor 0, ou seja,
+o algoritmo deve ficar calculando o maior até que a 
+entrada seja igual a 0 (zero).
+*/
+
+correção
+
+maior = 0
+recebe valor
+enquanto valor != 0, processar
+    Se valor > maior então
+        maior = valor 
+    recebe valor
+imprimir "Maior valor " + maior
---
+++ Section7/section7_exe02.py
@@ -0,0 +1,16 @@
+/*
+Section7_exe02
+
+Faça um algoritmo que conte de 1 a 100 e a cada 
+múltiplo de 10 emita uma mensagem: "Múltiplo de 10". 
+*/
+
+número = 1
+recebe número
+para número <= 100 processe
+    escrever i
+    número = número + 1
+    Se número % 10 == 1 então
+        imprimir número + " é múltiplo de 10"
+    fim se
+fim

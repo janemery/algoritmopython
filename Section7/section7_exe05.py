1c8359899e6fd349d94f6c33c1cde39175a5a3b5
Jane Ferreira <janymeryc@gmail.com>
2021-03-23 17:51:14 -0300

    adicionando exercicios 5 de seção 7

--- Section7/section7_exe05.py
+++ Section7/section7_exe05.py
@@ -1,3 +1,18 @@
 /*
 Section7_exe05
+
+Faça um programa que leia um nome de usuário e a sua
+senha e não aceite a senha igual ao nome de usuário,
+mostrando uma mensagem de erro e voltando a pedir as 
+informações
 */
+
+escrever "Informe o nome"
+receber nome
+escrever "Informe a senha"
+receber senha
+    Se senha == nome então
+        escrever "senha inválida"
+    sair
+    Senão
+        escrever "Senha foi salva com sucesso"

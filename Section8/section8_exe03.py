a3bf4c7383930af5af876af27661dd5f20d5e7eb
Jane Ferreira <janymeryc@gmail.com>
2021-03-24 17:44:23 -0300

    adicionando seção 8 exercicio 3

--- Section8/section8_exe03.py
+++ Section8/section8_exe03.py
@@ -1,4 +1,27 @@
 /*
-Section8_exe01
+Section8_exe03
+Faça um programa que carregue um vetor com 10 numeros
+inteiros. mostre o vetor na ordem inversa ao que foi digitado.
 
 */
+solução:
+
+vetor = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+para i = 0, enquanto i < 10 processe
+    escreva "digite o valor: "
+    recebe valor
+    vetor[i] = valor
+
+correção:
+
+vetor = []
+para i = 0, enquanto i <= 10, processar
+    escrever "Digite um valor: "
+    receber valor
+    vetor[i] = valor
+
+i = 9
+enquanto i >= 0 processar
+    escrever vetor[i]
+    i = i - 1
